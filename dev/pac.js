8217e149bd361ba66a8643225340c5ba8dee41c1
JinnLynn <JinnLynn@users.noreply.github.com>
2024-06-21 08:41:28 +0000

    update 2024-06-21 08:41:25 +0000

--- dev/pac.js
+++ dev/pac.js
@@ -1,6 +1,6 @@
 /**
  * genpac 3.1.0.dev1 https://github.com/JinnLynn/genpac
- * Generated: 2024-06-21 08:36:40
+ * Generated: 2024-06-21 08:41:21
  * GFWList Last-Modified: 2024-06-07 03:48:34
  * GFWList From: online[https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt]
  */
