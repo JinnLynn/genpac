711ad6cbdc2be2082e83301b5abbf1b3274e2873
JinnLynn <JinnLynn@users.noreply.github.com>
2024-06-30 12:33:38 +0000

    update 2024-06-30 12:33:37 +0000

--- master/pac-tiny.js
+++ master/pac-tiny.js
@@ -1,6 +1,6 @@
 /**
  * genpac 3.0.1 https://github.com/JinnLynn/genpac
- * Generated: 2024-06-30 01:26:48
+ * Generated: 2024-06-30 12:33:31
  * GFWList Last-Modified: 2017-11-16 03:07:48
  * GFWList From: online[https://raw.githubusercontent.com/gfwlist/tinylist/master/tinylist.txt]
  */
